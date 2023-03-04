7bea8d92ffc445a9227f4fa73ef61064aa8faaa3
hypolas <nicolas.hypolite@gmail.com>
2023-03-04 19:51:24 +0100

    First test

---
+++ json_test.go
@@ -0,0 +1,32 @@
+package hypolasjsondecomposer
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+// TestResolve test different strings format
+func TestResolve(t *testing.T) {
+	jso, _ := os.ReadFile("test/test.json")
+	logf.Info.Println(ReadJSONFromFlatPath("services", jso))
+	logf.Info.Println(ReadJSONFromFlatPath("services__1__status", jso))
+	logf.Info.Println(ReadJSONFromFlatPath("services__0__nom", jso))
+
+	readFile, err := os.Open(logf.LogFile.Name())
+
+	if err != nil {
+		panic(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
+}
